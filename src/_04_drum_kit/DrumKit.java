8ac8d12bbd6c4627d21e5485befa7e75566668f2
Tor <Tor@Jason.lan>
2020-11-18 18:28:10 -0800

    quiz

--- src/_04_drum_kit/DrumKit.java
+++ src/_04_drum_kit/DrumKit.java
@@ -25,44 +25,44 @@ public class DrumKit implements MouseListener {
 	public void run() throws MalformedURLException {
 
 		// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+		JFrame frame = new JFrame();
 		// 2. Make the frame visible and
 		// set its default close operation to JFrame.EXIT_ON_CLOSE
-
+		frame.setVisible(true);
 		// 3. Set the size of the frame
-
+		frame.setSize(500, 500);
 		// 4. Set the title of the frame
-
+		frame.setTitle("drumset");
 		// 5. Make a JPanel variable and initialize it using "new JPanel().
-
+		JPanel panel = new JPanel();
 		// 6. Add the panel to the frame. (The panel is invisible.)
-
+		frame.add(panel);
 		// 7. Download an image of a drum from the Internet. Drop it into your
 		// Eclipse project under "recipe package(drum_kit)".
-
+		
 		// 8. Put the name of your image file in a String variable.
-
+		String drumset = "drumset.jpg";
 		// 9. Edit the next line to use your String variable
 		// drumLabelWithImage = createLabelImage(drumImageString);
-
+		drumLabelWithImage = createLabelImage(drumset);
 		// 10. Add the image to the panel
-
+		panel.add(drumLabelWithImage);
 		// 11. Set the layout of the panel to "new GridLayout()"
-
+		panel.setLayout(new GridLayout());
 		// 12. call the pack() method on the frame. Run your program. Do you see
 		// your drum image?
-
+		frame.pack();
 		// 13. add this mouse listener to drumLabelWithImage
-
+		drumLabelWithImage.addMouseListener(this);
 		// 18. Add more images to make a drumkit. Remember to add this mouse
 		// listener to each one.
-
+		
 	}
 
 	public void mouseClicked(MouseEvent e) {
 		// 14. Print "mouse clicked" to the console. Run your program and watch
 		// the console to see when this is printed.
-
+		System.out.println("mouse clicked");
 		JLabel drumClicked = (JLabel) e.getSource(); // This line gets the label
 														// that the mouse
 														// clicked on
@@ -70,12 +70,12 @@ public class DrumKit implements MouseListener {
 		// 15. Download a drum sound and drop it into your "recipe package(drum_kit)".
 		// You can find it on freesound.org. To download it, log in as
 		// leagueofamazing/code4life.
-
+		
 		// 16. If they clicked on the drumImage...
-
+		
 		// 17. ...use the playSound method to play a drum sound. Test to see if
 		// it works
-
+		playSound("drum2.wav");
 	}
 
 	private JLabel createLabelImage(String fileName) throws MalformedURLException {
--- src/_05_body_part_quiz/BodyPartQuiz.java
+++ src/_05_body_part_quiz/BodyPartQuiz.java
@@ -51,23 +51,43 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+		int score = 0;
 		// 2. Set the size of the window in the initializeGui() method 
-
+		
 		// 4. Ask the user who this person is and store their answer
 		String guess = JOptionPane.showInputDialog("who is this?");
 
 		// 5. Check their answer. If they guessed correctly:
 		// -- Tell them they are right and increase the score by 1
-
+		if(guess.equalsIgnoreCase("arnold")) {
+			JOptionPane.showMessageDialog(null, "you guessed correctly, score+1");
+			score=score+1;
+		}
 		// 6. Otherwise:
 		// -- Tell them they are wrong and who the person is
-
+		else {
+			JOptionPane.showMessageDialog(null, "you guessed wrong, it was arnold");
+		}
 		// 7. Use the showNextImage() method below to get the next image
 		showNextImage();
-
+		
 		// 8. .... repeat 4-7 for all your images.....
-
+		if(guess.equalsIgnoreCase("leonardo")) {
+			JOptionPane.showMessageDialog(null, "you guessed correctly, score+1");
+			score=score+1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "you guessed wrong, it was leonardo");
+		}
+		showNextImage();
+		if(guess.equalsIgnoreCase("jack")) {
+			JOptionPane.showMessageDialog(null, "you guessed correctly, score+1");
+			score=score+1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "you guessed wrong, it was jack");
+		}
+		showNextImage();
 		// 9. Show them their current score
 
 	}
